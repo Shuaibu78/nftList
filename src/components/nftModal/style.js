832df2ef02378920aa947d21c394a8d4043e4dab
Shuaibu Muhammad <devshuaib@gmail.com>
2023-03-08 13:09:21 +0100

    removed commented code

--- src/components/nftModal/style.js
+++ src/components/nftModal/style.js
@@ -1,43 +1,5 @@
 import styled from "styled-components";
 
-// export const ModalWrapper = styled.div`
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   background-color: rgba(0, 0, 0, 0.5);
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// export const Modal = styled.div`
-//   background-color: white;
-//   word-wrap: break-word;
-//   padding: 20px;
-//   border-radius: 10px;
-//   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
-
-//   img {
-//     width: 520px;
-//     height: 400px;
-//   }
-
-//   p {
-//     max-width: 520px;
-//   }
-// `;
-
-// export const Button = styled.a`
-//   background-color: #2ecc71;
-//   color: white;
-//   padding: 10px;
-//   border-radius: 5px;
-//   text-decoration: none;
-//   text-align: center;
-// `;
-
 export const ModalWrapper = styled.div`
   position: fixed;
   top: 0;
