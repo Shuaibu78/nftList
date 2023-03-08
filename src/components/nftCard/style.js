c904640c528a0811b99f86e1da4d8a02bb1801e5
Shuaibu Muhammad <devshuaib@gmail.com>
2023-03-08 11:41:04 +0100

    made responsive changes

--- src/components/nftCard/style.js
+++ src/components/nftCard/style.js
@@ -1,10 +1,35 @@
 import styled from "styled-components";
 
+// export const CardGrid = styled.div`
+//   display: grid;
+//   grid-template-columns: auto auto auto;
+//   grid-gap: 20px;
+//   width: 100%;
+// `;
+
+// export const Card = styled.div`
+//   background-color: white;
+//   padding: 20px;
+//   border-radius: 10px;
+//   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+//   cursor: pointer;
+//   width: 400px;
+
+//   img {
+//     width: 100%;
+//     height: 400px;
+//   }
+// `;
+
 export const CardGrid = styled.div`
   display: grid;
-  grid-template-columns: auto auto auto;
+  grid-template-columns: auto;
   grid-gap: 20px;
   width: 100%;
+
+  @media (min-width: 768px) {
+    grid-template-columns: auto auto auto;
+  }
 `;
 
 export const Card = styled.div`
@@ -13,10 +38,18 @@ export const Card = styled.div`
   border-radius: 10px;
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
   cursor: pointer;
-  width: 400px;
+  width: 100%;
 
   img {
     width: 100%;
-    height: 400px;
+    height: auto;
+  }
+
+  @media (min-width: 768px) {
+    width: 400px;
+
+    img {
+      height: 400px;
+    }
   }
 `;
--- src/components/nftModal/style.js
+++ src/components/nftModal/style.js
@@ -1,5 +1,43 @@
 import styled from "styled-components";
 
+// export const ModalWrapper = styled.div`
+//   position: fixed;
+//   top: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 100%;
+//   background-color: rgba(0, 0, 0, 0.5);
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// export const Modal = styled.div`
+//   background-color: white;
+//   word-wrap: break-word;
+//   padding: 20px;
+//   border-radius: 10px;
+//   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+
+//   img {
+//     width: 520px;
+//     height: 400px;
+//   }
+
+//   p {
+//     max-width: 520px;
+//   }
+// `;
+
+// export const Button = styled.a`
+//   background-color: #2ecc71;
+//   color: white;
+//   padding: 10px;
+//   border-radius: 5px;
+//   text-decoration: none;
+//   text-align: center;
+// `;
+
 export const ModalWrapper = styled.div`
   position: fixed;
   top: 0;
@@ -10,6 +48,10 @@ export const ModalWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 768px) {
+    align-items: flex-start;
+  }
 `;
 
 export const Modal = styled.div`
@@ -20,12 +62,16 @@ export const Modal = styled.div`
   box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
 
   img {
-    width: 520px;
-    height: 400px;
+    width: 100%;
+    height: auto;
   }
 
   p {
-    max-width: 520px;
+    max-width: 100%;
+  }
+
+  @media (max-width: 768px) {
+    width: 90%;
   }
 `;
 
@@ -36,4 +82,9 @@ export const Button = styled.a`
   border-radius: 5px;
   text-decoration: none;
   text-align: center;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 10px;
+  }
 `;
